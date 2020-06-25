014cd64c51132eb414dbdec5da008118702ba4d9
Karma <chuongchung.it@gmail.com>
2020-06-25 19:29:36 +0700

    Create Area Admin & config MapRoute for Admin

--- KarmaCoreApp/Startup.cs
+++ KarmaCoreApp/Startup.cs
@@ -96,6 +96,10 @@ namespace KarmaCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
 
             //dbInitializer.Seed().Wait();
