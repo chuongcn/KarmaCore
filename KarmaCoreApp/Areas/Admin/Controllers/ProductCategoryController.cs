8f2384855e3bd77fd05baeb78599d071e9e041eb
Karmar <chuongchung.it@gmail.com>
2020-11-16 17:35:04 +0700

    Lesson15_ Display category tree

---
+++ KarmaCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using KarmaCoreApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KarmaCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Data API
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+
+        #endregion Get Data API
+    }
+}
---
+++ KarmaCoreApp/wwwroot/app/controllers/product-category/index.js
@@ -0,0 +1,32 @@
+﻿var productCategoryController = function () {
+    this.initialize = function () {
+        loadData();
+    }
+
+    function loadData() {
+        $.ajax({
+            type: 'GET',
+            url: '/Admin/ProductCategory/GetAll',
+            datatype: 'json',
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder,
+                    });
+                });
+                var treeArr = karmar.unflattern(data);
+
+                //var tree = $('#treeProductCategory');
+
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd: true
+                });
+            },
+        });
+    }
+}
